2a53345994b5e1a7c0dfdb215beaf7e4325932f3
Lucas Ceruti <lucas.ceeruti@gmail.com>
2023-03-13 14:59:24 -0300

    Criado Rotas

--- src/index.js
+++ src/index.js
@@ -1,9 +1,31 @@
-const express = require('express')
+import * as express from 'express'
 
 const app = express()
 
+/*
+ * app.get(parametro = '/rota', função que vai processar () => {
+    res.send (responde com status e mostra para o usuario)
+ })
+ */
+
 app.get('/', (req, res) => {
-    res.send('Hello World')
+    res.send('GET /')
+})
+
+app.post('/', (req, res) => {
+    res.status(201).send('POST /')
+})
+
+app.put('/', (req, res) => {
+    res.status(400).send('PUT /')
+})
+
+app.patch('/', (req, res) => {
+    res.send('PATCH /')
+})
+
+app.delete('/', (req, res) => {
+    res.send('DELETE /')
 })
 
 app.listen(3000, () => console.log(`ONLINE http://localhost:3000`))
