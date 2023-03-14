69d271d24ca36d36460594b488d2b34127c44a1a
Lucas Ceruti <lucas.ceeruti@gmail.com>
2023-03-14 15:51:14 -0300

    Criado login e autenticação com JWT

--- src/modules/user/userService.js
+++ src/modules/user/userService.js
@@ -1,5 +1,9 @@
+import { sign } from "jsonwebtoken"
+
 
 let users = []
+
+const generateAcessToken = (data) => sign(data, 'secret')
 
 const getUserByEmail = (searchEmail) => 
 users.find((obj) => obj.email === searchEmail)
@@ -8,5 +12,14 @@ export const signup = (data) => {
     if(getUserByEmail(data.email)) throw new Error('email_existente')
 
     users.push(data)
-    return true
+    return generateAcessToken({ email: data.email })
+}
+
+export const login = (data) => {
+    const user = getUserByEmail(data.email)
+    if(!user) throw new Error('email_nao_encontrado!')
+
+    if(user.password !== data.password) throw new Error('senha_incorreta')
+
+    return generateAcessToken({ email: data.email })
 }
