cf3ba8c46b328e2d6783f8f7942a00e33dfc35f8
Lucas Ceruti <lucas.ceeruti@gmail.com>
2023-03-14 15:09:40 -0300

    Criado tratativa de errors para signup

--- src/modules/user/userService.js
+++ src/modules/user/userService.js
@@ -5,11 +5,8 @@ const getUserByEmail = (searchEmail) =>
 users.find((obj) => obj.email === searchEmail)
 
 export const signup = (data) => {
-    if(getUserByEmail(data.email)) {
-        console.log('EXISTE EMAIL')
-    }else {
-        users.push(data)
-    }
+    if(getUserByEmail(data.email)) throw new Error('email_existente')
 
+    users.push(data)
     return true
 }
